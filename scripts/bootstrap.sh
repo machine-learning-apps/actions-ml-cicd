cf848b9ce163278b3d296bfd9cb097d43d0c0927
Hamel Husain <hamel.husain@gmail.com>
2019-09-17 11:14:00 -0700

    updated scripts

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,4 +4,6 @@ set -e
 cd $(dirname "$0")/..
 
 docker build -t hamelsmu/ml-cicd -f docker/Dockerfile .
-docker run -e DASK_SCHEDULER=$DASK_SCHEDULER -it -p 8888:8888 -v $PWD:/data hamelsmu/ml-cicd bash
+docker-compose up -d
+docker exec -it hamelsmu/ml-cicd bash
+#docker run -e DASK_SCHEDULER=$DASK_SCHEDULER -it -p 8888:8888 -v $PWD:/data hamelsmu/ml-cicd bash
