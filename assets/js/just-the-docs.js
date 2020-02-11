058c39a19b22d4bbddc0991e221f7a240cb25ca6
hamelsmu <hamelsmu@users.noreply.github.com>
2020-02-11 21:49:50 +0000

    Automated deployment: Tue Feb 11 21:49:50 UTC 2020 378caafa8f10cda6935768ca592ae236b3e424e7

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -44,7 +44,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'https://machine-learning-apps.github.io/assets/js/search-data.json', true);
+  request.open('GET', 'https://machine-learning-apps.github.io/actions-ml-cicd/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
